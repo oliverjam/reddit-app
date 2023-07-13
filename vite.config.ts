b4f9e571db80ebafc8d3d5e2ee99b4d781db3336
oliverjam <oliverjamesphillips@gmail.com>
2023-07-13 12:04:13 +0100

    Ensure most JS stays cached for longer
    By splitting out dependencies into a vendor chunk

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   server: { port: 3000 },
   build: { target: "esnext" },
 });
