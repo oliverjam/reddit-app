56f6fe9b9375121dfd4f072f15fc71457351e9a2
oli <oli@supply25.com>
2025-05-11 16:03:23 +0100

    Fix type validation errors
    Reddit started returning "gallery" for post_hint,
    which was causing validation errors.
    
    Also upgrade Valibot to 1.0, which required a bit
    of a refactor to the types.

--- src/reddit/types.ts
+++ src/reddit/types.ts
@@ -1,26 +1,26 @@
 import {
-  array,
-  boolean,
-  literal,
-  merge,
-  nullable,
-  number,
-  object,
-  optional,
-  parse as _parse,
-  picklist,
-  record,
-  recursive,
-  string,
-  tuple,
-  union,
+	array,
+	boolean,
+	literal,
+	nullable,
+	nullish,
+	record,
+	number,
+	object,
+	optional,
+	parse as _parse,
+	picklist,
+	lazy,
+	string,
+	tuple,
+	union,
 } from "valibot";
-import { type BaseSchema, type Output } from "valibot";
+import { type GenericSchema, type InferOutput } from "valibot";
 export { parse } from "valibot";
 
-export type Link = Output<typeof Link>;
-export type Comment = Output<typeof Comment>;
-export type Kind = Output<typeof Kind>;
+export type Link = InferOutput<typeof Link>;
+export type Comment = InferOutput<typeof Comment>;
+export type Kind = InferOutput<typeof Kind>;
 
 /**
  * Base API definition
@@ -28,15 +28,15 @@ export type Kind = Output<typeof Kind>;
  */
 
 const Base = object({
-  id: string(),
-  permalink: string(),
-  subreddit: string(),
-  author: string(),
-  created_utc: number(),
-  score: number(),
-  ups: number(),
-  downs: number(),
-  stickied: boolean(),
+	id: string(),
+	permalink: string(),
+	subreddit: string(),
+	author: string(),
+	created_utc: number(),
+	score: number(),
+	ups: number(),
+	downs: number(),
+	stickied: boolean(),
 });
 
 /**
@@ -45,15 +45,15 @@ const Base = object({
  */
 
 const More = object({
-  kind: literal("more"),
-  data: object({
-    count: number(),
-    name: string(),
-    id: string(),
-    parent_id: string(),
-    depth: number(),
-    children: array(string()),
-  }),
+	kind: literal("more"),
+	data: object({
+		count: number(),
+		name: string(),
+		id: string(),
+		parent_id: string(),
+		depth: number(),
+		children: array(string()),
+	}),
 });
 
 /**
@@ -61,41 +61,41 @@ const More = object({
  * This describes a comment on Reddit
  */
 
-const BaseCommentData = merge([
-  Base,
-  object({
-    link_title: optional(string()),
-    link_permalink: optional(string()),
-    link_id: optional(string()),
-    body: string(),
-    body_html: string(),
-    is_submitter: boolean(),
-    distinguished: nullable(literal("moderator")),
-  }),
-]);
+const BaseCommentData = object({
+	...Base.entries,
+	...object({
+		link_title: optional(string()),
+		link_permalink: optional(string()),
+		link_id: optional(string()),
+		body: string(),
+		body_html: string(),
+		is_submitter: boolean(),
+		distinguished: nullable(literal("moderator")),
+	}).entries,
+});
 
 // TypeScript cannot infer recursive type, so we define it manually
-type CommentData = Output<typeof BaseCommentData> & {
-  replies: "" | Output<typeof CommentListing>;
+type CommentData = InferOutput<typeof BaseCommentData> & {
+	replies: "" | InferOutput<typeof CommentListing>;
 };
 
-const CommentData: BaseSchema<CommentData> = merge([
-  BaseCommentData,
-  object({
-    replies: union([literal(""), recursive(() => CommentListing)]),
-  }),
-]);
+const CommentData: GenericSchema<CommentData> = object({
+	...BaseCommentData.entries,
+	...object({
+		replies: union([literal(""), lazy(() => CommentListing)]),
+	}).entries,
+});
 
 const Comment = object({
-  kind: literal("t1"),
-  data: CommentData,
+	kind: literal("t1"),
+	data: CommentData,
 });
 
 const CommentListing = object({
-  kind: literal("Listing"),
-  data: object({
-    children: array(union([Comment, More])),
-  }),
+	kind: literal("Listing"),
+	data: object({
+		children: array(union([Comment, More])),
+	}),
 });
 
 /**
@@ -104,93 +104,125 @@ const CommentListing = object({
  */
 
 const Source = object({
-  url: string(),
-  width: number(),
-  height: number(),
+	url: string(),
+	width: number(),
+	height: number(),
 });
 
-const LinkData = merge([
-  Base,
-  object({
-    title: string(),
-    url: string(),
-    is_self: boolean(),
-    selftext: string(),
-    selftext_html: nullable(string()),
-    num_comments: number(),
-    thumbnail: string(),
-    thumbnail_width: optional(nullable(number())),
-    thumbnail_height: optional(nullable(number())),
-    post_hint: optional(
-      picklist(["link", "image", "hosted:video", "rich:video", "self"]),
-    ),
-    domain: string(),
-    is_video: boolean(),
-    preview: optional(object({
-      images: array(object({
-        source: Source,
-        resolutions: array(object({
-          url: string(),
-          width: number(),
-          height: number(),
-        })),
-        variants: object({
-          gif: optional(object({ source: Source })),
-          mp4: optional(object({ source: Source })),
-        }),
-      })),
-      reddit_video_preview: optional(object({
-        fallback_url: string(),
-        width: number(),
-        height: number(),
-      })),
-    })),
-    media: nullable(optional(object({
-      oembed: optional(object({
-        provider_name: string(),
-        thumbnail_url: optional(string()),
-        width: number(),
-        height: nullable(number()),
-        html: string(),
-      })),
-      reddit_video: optional(object({
-        fallback_url: string(),
-        width: number(),
-        height: number(),
-      })),
-    }))),
-    gallery_data: nullable(optional(object({
-      items: array(object({
-        media_id: string(),
-      })),
-    }))),
-    media_metadata: nullable(optional(record(object({
-      s: object({
-        x: number(),
-        y: number(),
-        u: optional(string()),
-				gif: optional(string()),
-				mp4: optional(string()),
-      }),
-    })))),
-  }),
-]);
+const LinkData = object({
+	...Base.entries,
+	...object({
+		title: string(),
+		url: string(),
+		is_self: boolean(),
+		selftext: string(),
+		selftext_html: nullable(string()),
+		num_comments: number(),
+		thumbnail: string(),
+		thumbnail_width: nullish(number()),
+		thumbnail_height: nullish(number()),
+		post_hint: optional(
+			picklist([
+				"link",
+				"image",
+				"hosted:video",
+				"rich:video",
+				"self",
+				"gallery",
+			])
+		),
+		domain: string(),
+		is_video: boolean(),
+		preview: optional(
+			object({
+				images: array(
+					object({
+						source: Source,
+						resolutions: array(
+							object({
+								url: string(),
+								width: number(),
+								height: number(),
+							})
+						),
+						variants: object({
+							gif: optional(object({ source: Source })),
+							mp4: optional(object({ source: Source })),
+						}),
+					})
+				),
+				reddit_video_preview: optional(
+					object({
+						fallback_url: string(),
+						width: number(),
+						height: number(),
+					})
+				),
+			})
+		),
+		media: nullable(
+			optional(
+				object({
+					oembed: optional(
+						object({
+							provider_name: string(),
+							thumbnail_url: optional(string()),
+							width: number(),
+							height: nullable(number()),
+							html: string(),
+						})
+					),
+					reddit_video: optional(
+						object({
+							fallback_url: string(),
+							width: number(),
+							height: number(),
+						})
+					),
+				})
+			)
+		),
+		gallery_data: nullish(
+			object({
+				items: array(
+					object({
+						media_id: string(),
+					})
+				),
+			})
+		),
+		media_metadata: nullish(
+			record(
+				string(),
+				object({
+					s: object({
+						x: number(),
+						y: number(),
+						u: optional(string()),
+						gif: optional(string()),
+						mp4: optional(string()),
+					}),
+				})
+			)
+		),
+	}).entries,
+});
 
 const Link = object({
-  kind: literal("t3"),
-  data: merge([
-    LinkData,
-    object({
-      crosspost_parent_list: optional(array(LinkData)), // avoids recursive type
-    }),
-  ]),
+	kind: literal("t3"),
+	data: object({
+		...LinkData.entries,
+		...object({
+			crosspost_parent_list: optional(array(LinkData)), // avoids recursive type
+		}).entries,
+	}),
 });
 
 export const LinkListing = object({
-  kind: literal("Listing"),
-  data: object({
-    children: array(Link),
-  }),
+	kind: literal("Listing"),
+	data: object({
+		children: array(Link),
+	}),
 });
 
 /**
@@ -200,10 +232,10 @@ export const LinkListing = object({
 const Kind = union([Comment, Link, More]);
 
 export const Listing = object({
-  kind: literal("Listing"),
-  data: object({
-    children: array(Kind),
-  }),
+	kind: literal("Listing"),
+	data: object({
+		children: array(Kind),
+	}),
 });
 
 /**
@@ -214,34 +246,36 @@ export const Listing = object({
 export const Post = tuple([LinkListing, CommentListing]);
 
 export type PostKind =
-  | "link"
-  | "video"
-  | "image"
-  | "gif"
-  | "gallery"
-  | "embed"
-  | "self"
-  | "crosspost";
+	| "link"
+	| "video"
+	| "image"
+	| "gif"
+	| "gallery"
+	| "embed"
+	| "self"
+	| "crosspost";
 
 export function parse_kind(data: Link["data"]): PostKind {
 	if (data.is_self) return "self";
-  if (data.gallery_data) return "gallery";
-  if (data.is_video) return "video";
-  if (data.preview?.images[0]?.variants?.mp4) return "gif";
-  if (data.media?.oembed) return "embed";
-  if (data.preview?.reddit_video_preview) return "video";
-  if (data.crosspost_parent_list) return "crosspost";
-  switch (data.post_hint) {
-    case "hosted:video":
-    case "rich:video":
-      return "video";
-    case "link":
-      return "link";
-    case "image":
-      return "image";
-    case "self":
-      return "self";
-  }
-  if (data.domain !== window.location.hostname) return "link";
-  return "self";
+	if (data.gallery_data) return "gallery";
+	if (data.is_video) return "video";
+	if (data.preview?.images[0]?.variants?.mp4) return "gif";
+	if (data.media?.oembed) return "embed";
+	if (data.preview?.reddit_video_preview) return "video";
+	if (data.crosspost_parent_list) return "crosspost";
+	switch (data.post_hint) {
+		case "hosted:video":
+		case "rich:video":
+			return "video";
+		case "link":
+			return "link";
+		case "gallery":
+			return "image";
+		case "image":
+			return "image";
+		case "self":
+			return "self";
+	}
+	if (data.domain !== window.location.hostname) return "link";
+	return "self";
 }
